9515cd2fc24682216a0d6926a82853a5fa2a2883
Rayan Moh <2865753m@student.gla.ac.uk>
2024-02-23 23:15:58 +0000

    Base.html template and base.css added.

---
+++ typerush/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from typerush import views
+
+app_name = 'typerush'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- typerush_project/urls.py
+++ typerush_project/urls.py
@@ -1,4 +1,4 @@
-"""tango_with_django_project URL Configuration
+"""typerush_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -24,6 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
+    path('typerush/', include('typerush.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
