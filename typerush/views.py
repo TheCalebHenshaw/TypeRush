25c651e4a529a51d27415d0328855a2b93c32f13
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-02-29 16:00:02 +0000

    Local changes

--- typerush/views.py
+++ typerush/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
