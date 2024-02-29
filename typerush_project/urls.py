82c28ab8aa1d9487963998aa5e48260f3ba28611
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-02-29 20:12:07 +0000

    default page is now the home

--- typerush_project/urls.py
+++ typerush_project/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rango import views
+from typerush import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('typerush/', include('typerush.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
