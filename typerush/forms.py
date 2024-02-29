dbf53c257095859bb5f772060f78bdd505c044c4
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-02-29 15:49:19 +0000

    forms

---
+++ typerush/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from rango.models import Page,Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
+
