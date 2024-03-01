78acc475dd4042eb0366d6281ae8b14067bd780b
Mijuku <2840495c@student.gla.ac.uk>
2024-03-01 20:27:27 +0000

    Login html+css

--- typerush/urls.py
+++ typerush/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('profile/', views.profile, name='profile'),
-    path('edit-profile/', views.edit_profile, name='edit_profile')
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('login/', views.user_login, name='user_login'),
+    path('register/', views.register, name='register'),
 ]
