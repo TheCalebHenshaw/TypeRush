2dc594c043a6a915e1bf4568a42e2e10948e0cb8
“Eduardo <2840598F@student.gla.ac.uk>
2024-03-06 21:23:48 +0000

    reload popup to-fix

--- static/script/JQuery.js
+++ static/script/JQuery.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+    window.onbeforeunload = function() {
+        return "Are you sure you want to leave?";
+    };
+
     $(".get-started-button").click(function() {
         $(this).animate({opacity: 0}, 100);
         $(this).css("margin-left", "1000px"); 
-         
     });
 });
