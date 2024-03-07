f1253e4114e3a9d481c6e8701a96f63307881b79
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-03-07 15:54:05 +0000

    changed populate_typerush

--- populate_typerush.py
+++ populate_typerush.py
@@ -12,18 +12,27 @@ def populate():
  # Create users
     user1 = User.objects.create_user(username='user1', password='password1')
     user2 = User.objects.create_user(username='user2', password='password2')
-    #user3 = User.objects.create_user(username=)
+    user3 = User.objects.create_user(username='user3',password='password3')
+    user4 = User.objects.create_user(username='user4',password='password4')
+    user5 = User.objects.create_user(username='user5',password='password5')
+    user6 = User.objects.create_user(username='user6',password='password6')
+    user7 = User.objects.create_user(username='user7',password='password7')
+    user8 = User.objects.create_user(username='user8',password='password8')
     # Populate Player model
-    player1 = Player.objects.create(user=user1, firstname='John', surname='Doe', country='USA', total_races=10, average_speed=50, rank=1)
-    player2 = Player.objects.create(user=user2, firstname='Jane', surname='Smith', country='Canada', total_races=8, average_speed=45, rank=2)
+    player1 = Player.objects.create(user=user1, firstname='John', surname='Doe', country='USA', total_races=10, average_speed=50)
+    player2 = Player.objects.create(user=user2, firstname='Jane', surname='Smith', country='Canada', total_races=8, average_speed=45)
 
     # Populate Leaderboard model
-    leaderboard = Leaderboard.objects.create(title='Main Leaderboard')
-    leaderboard.players.add(player1, player2)
+    easyleaderboard = Leaderboard.objects.create(title='Easy Leaderboard')
+    mediumleaderboard = Leaderboard.objects.create(title='Medium Leaderboard')
+    hardleaderboard = Leaderboard.objects.create(title='Hard Leaderboard')
+
+    easyleaderboard.players.add(player1, player2)
 
     # Populate Mode model
-    mode = Mode.objects.create(difficulty='Easy', races_played=20, highscore=100, leaderboard=leaderboard)
-
+    easyMode = Mode.objects.create(difficulty='Easy', races_played=20, highscore=50, leaderboard=easyleaderboard)
+    mediumMode = Mode.objects.create(difficulty='Medium',races_played=17,highscore=33,leaderboard=mediumleaderboard)
+    hardMode = Mode.objects.create(difficulty='Hard', races_played=15,highscore=22,leaderboard=hardleaderboard)
     # Populate Game model
     game1 = Game.objects.create(mode=mode, userID=player1, score=80)
     game2 = Game.objects.create(mode=mode, userID=player2, score=75)
