f597e3b1dd8198690a1311da8223b462a49a0fdd
Rayan Moh <2865753m@student.gla.ac.uk>
2024-03-15 21:41:34 +0000

    2637 register commit

--- typerush/forms.py
+++ typerush/forms.py
@@ -1,25 +1,47 @@
 from django import forms
-from rango.models import Page,Category
 from django.contrib.auth.models import User
-from rango.models import UserProfile
 from typerush.models import Player
 
 class UserForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        # Dictionary mapping field names to custom IDs
+        custom_ids = {
+            'username': 'username',
+            'password': 'password',
+        }
+        # Assign custom IDs to form fields
+        for field_name, custom_id in custom_ids.items():
+            self.fields[field_name].widget.attrs['id'] = custom_id
+
     username = forms.CharField(required=True)
-    password = forms.CharField(required=True,widget=forms.PasswordInput())
+    password = forms.CharField(required=True)
     
     class Meta:
         model = User
         fields = ('username', 'password',)
 
 class UserProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        # Dictionary mapping field names to custom IDs
+        custom_ids = {
+            'firstname': 'firstname',
+            'surname': 'surname',
+            'country': 'country',
+            'profile_picture': 'profile_picture',
+        }
+        # Assign custom IDs to form fields
+        for field_name, custom_id in custom_ids.items():
+            self.fields[field_name].widget.attrs['id'] = custom_id
+    
     firstname = forms.CharField(max_length=100)
-    lastname = forms.CharField(max_length=100)
+    surname = forms.CharField(max_length=100)
     country = forms.Select()
     profile_picture = forms.ImageField(required=False)
     class Meta:
         model = Player
-        fields = ('firstname','lastname','country','profile_picture',)
+        fields = ('firstname','surname','country','profile_picture',)
 
 class EditUserForm(forms.ModelForm):
     username = forms.CharField(widget=forms.PasswordInput())
