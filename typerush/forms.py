64c6611dcc65b8ed45d74105090489971615e11f
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-03-15 15:01:18 +0000

    edit forms

--- typerush/forms.py
+++ typerush/forms.py
@@ -2,16 +2,27 @@ from django import forms
 from rango.models import Page,Category
 from django.contrib.auth.models import User
 from rango.models import UserProfile
+from typerush.models import Player
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'password',)
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
-        fields = ('website', 'picture',)
+        model = Player
+        fields = ('firstname','surname','country','profile_picture',)
+
+class EditUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username','password',)
+class EditPlayerProfileForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ('profile_picture')
 
