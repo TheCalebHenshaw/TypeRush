45b074fb6e0afe51ecffa661b3306bbc25d6f01a
“Eduardo <eduardogabrielflores@gmail.com>
2024-03-17 15:46:30 +0000

    Added popup to logout

--- static/script/JQuery.js
+++ static/script/JQuery.js
@@ -4,3 +4,9 @@ $(".get-started-button").click(function() {
     $(this).animate({opacity: 0}, 100);
     $(this).css("margin-left", "1001px"); 
 });
+
+$("#logoutButton").on('click', function(event){
+    window.onbeforeunload = () => {
+        return "Are you sure you want to log out?";
+    };
+});
